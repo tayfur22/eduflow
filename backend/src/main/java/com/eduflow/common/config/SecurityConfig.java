ae61a033c334a02b5cb60d4e036f40f84ccf0740
tayfur22 <tayfurtagizade23@gmail.com>
2026-05-19 00:49:19 +0400

    add swagger

--- backend/src/main/java/com/eduflow/common/config/SecurityConfig.java
+++ backend/src/main/java/com/eduflow/common/config/SecurityConfig.java
@@ -42,6 +42,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/auth/**",
                                 "/api/courses/public/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
                                 "/api/certificates/verify/**"   // ← əlavə edildi
                         ).permitAll()
                         .anyRequest().authenticated()
